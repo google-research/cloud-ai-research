93926a335041f4dd39c76a7ce6958aeede275dfc
Raj Sinha <sinharaj@google.com>
2024-09-17 08:30:27 -0700

    Small update to Github site config and one simple unit test.
    PiperOrigin-RevId: 675578499

---
+++ cloud_ai_research/init_test.py
@@ -0,0 +1,47 @@
+# Copyright 2024 The cloud_ai_research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding=utf-8
+# Copyright 2024 The Cloud AI Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Basic test for the __init__ file."""
+
+import unittest
+
+import cloud_ai_research
+
+
+import pytest
+
+
+class InitTest(unittest.TestCase):
+
+  def test_version_is_set(self):
+    self.assertIsNotNone(cloud_ai_research.__version__)
+
+
+if __name__ == "__main__":
+  unittest.main()
